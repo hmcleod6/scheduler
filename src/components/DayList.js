5a8171474c9a0d712826aa99ad94c1222e57bd8a
Heather <hmcleod29@gmail.com>
2022-06-14 19:20:24 -0400

    Build DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,6 +3,19 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  return <ul></ul>
-  
+  const renderDays = props.days.map(day => {
+    return (
+    <DayListItem 
+      key={day.id}
+      id={day.id}  
+      name={day.name}
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />)
+    });
+
+  return(
+   <ul>{renderDays}</ul>
+  );
 }
